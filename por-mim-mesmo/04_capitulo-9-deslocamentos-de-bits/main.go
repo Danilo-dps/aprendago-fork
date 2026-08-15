e07ebbd409542bb35989b0b203e04eeffa45540c
Danilo Pereira da Silva <danilodpsnilo@outlook.com>
2026-08-15 18:50:18 -0300

    feat: deslocamentos de bits

---
+++ por-mim-mesmo/04_capitulo-9-deslocamentos-de-bits/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	_  = iota             // 0
+	KB = 1 << (iota * 10) // 1 << (1 * 10) [ 1 << (iota * 10) essa expressão não precisaria colocar de forma explicita por o iota resolve isso]
+	MB = 1 << (iota * 10) // 1 << (2 * 10)
+	GB = 1 << (iota * 10) // 1 << (3 * 10)
+	TB = 1 << (iota * 10) // 1 << (4 * 10)
+)
+
+func main() {
+	fmt.Println("binary\t\t\t\t\t\tdecimal")
+	fmt.Printf("%b\t\t\t\t\t", KB)
+	fmt.Printf("%d\n", KB)
+	fmt.Printf("%b\t\t\t\t", MB)
+	fmt.Printf("%d\n", MB)
+	fmt.Printf("%b\t\t\t", GB)
+	fmt.Printf("%d\n", GB)
+	fmt.Printf("%b\t", TB)
+	fmt.Printf("%d\n", TB)
+}
