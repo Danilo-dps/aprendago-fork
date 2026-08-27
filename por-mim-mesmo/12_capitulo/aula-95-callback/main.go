3dbc64635ba3b19cacc729574d3765b2b2743f88
Danilo Pereira da Silva <danilodpsnilo@outlook.com>
2026-08-27 00:21:46 -0300

    feat: callback

---
+++ por-mim-mesmo/12_capitulo/aula-95-callback/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	numerosPares := somentePares(soma, []int{50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60}...)
+	fmt.Println(numerosPares)
+
+	fmt.Println()
+	numerosImpares := somenteImpares(soma, []int{50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60}...)
+	fmt.Println(numerosImpares)
+}
+
+func soma(x ...int) int {
+	n := 0
+	for _, v := range x {
+		n += v
+	}
+	return n
+}
+
+func somentePares(f func(x ...int) int, y ...int) int {
+	var slice []int
+	for _, v := range y {
+		if v%2 == 0 {
+			slice = append(slice, v)
+		}
+	}
+	total := f(slice...)
+	return total
+}
+
+func somenteImpares(f func(x ...int) int, y ...int) int {
+	var slice []int
+	for _, v := range y {
+		if v%2 != 0 {
+			slice = append(slice, v)
+		}
+	}
+	total := f(slice...)
+	return total
+}
